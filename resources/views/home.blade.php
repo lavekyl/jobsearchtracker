e2f17b4e5dda5b91cef9cda4ba1bc0bc0472a3e8
Kyle Laverty <lavekyl@gmail.com>
2023-08-17 17:00:45 -0400

    content updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
   <section class="bg-gray-50 dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
       <div class="flex flex-col justify-center">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Track Your Job Search</h1>
+        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Applying for jobs can become difficult to track, so we've created a tool to make it easier. Say goodbye spreadsheet trackers.</p>
         <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more about our app 
           <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -14,7 +14,7 @@
       </div>
       <div>
         <div class="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
-          <h2 class="text-2xl font-bold text-gray-900 dark:text-white">Sign in to Flowbite</h2>
+          <h2 class="text-2xl font-bold text-gray-900 dark:text-white">Sign in</h2>
           <form class="mt-8 space-y-6" action="#">
             <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
